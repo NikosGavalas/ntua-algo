8f48300a039de41dc53bd8b37090f631122f551c
Nick Gavalas <gavalnick@gmail.com>
2018-10-30 00:51:32 +0200

    ex1b is full of bugs, added handy script for running tests

---
+++ ex1b/run.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+PARTICLES_TESTCASES_DIR="particles_testcases"
+TEST_NUM="1"
+VERBOSE="true"
+
+if [ ! -z "$1" ]; then
+    TEST_NUM=$1;
+fi
+
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+RST='\033[0m'
+
+# compile
+g++ -g -o particles particles.cpp
+
+# run tests
+for i in $(seq 1 ${TEST_NUM})
+do
+    result=$(diff ${PARTICLES_TESTCASES_DIR}/output${i}.txt <(cat ${PARTICLES_TESTCASES_DIR}/input${i}.txt | ./particles))
+    if [[ result != 0 ]]; then
+        echo -e "Test ${i} ${RED}failed${RST}"
+        if [[ $VERBOSE == "true" ]]; then
+            echo -e "${YELLOW}Output:${RST}"
+            cat ${PARTICLES_TESTCASES_DIR}/input${i}.txt | ./particles
+            echo -e "${YELLOW}Expected output:${RST}"
+            cat ${PARTICLES_TESTCASES_DIR}/output${i}.txt
+        fi
+        echo "start debugging?[y/n]"
+        read answer
+        if [[ $answer == "y" || $answer == "Y" ]]; then
+           gdb particles
+           exit 1
+        fi
+    else
+        echo -e "Test ${i} ${GREEN}passed${RST}"
+    fi
+done
