5e9ce1faa46624b7535d4d23619d99f2f2d59fb0
Nick Gavalas <gavalnick@gmail.com>
2018-10-31 16:01:17 +0200

    ex1b 'particles' completed with the O(KNlogL) algorithm

--- ex1b/particles.cpp
+++ ex1b/particles.cpp
@@ -37,27 +37,37 @@ public:
     }
 
     // O(1)
-    int position_at_time(int T)
+    double position_at_time(double T)
     {
+        double ret;
+
         if (this->team == A)
-            return this->velocity * (T - this->start);
-        return (this->velocity * (this->start - T)) + L;
+            ret = this->velocity * (T - this->start);
+        else
+            ret = (this->velocity * (this->start - T)) + L;
+
+        if (ret < 0)
+            return 0;
+        if (ret > L)
+            return L;
+        return ret;
     }
 };
 
+// heap allocated, I know it's bad but I expect these to grow too much
 vector<Particle*>* a_particles;
 vector<Particle*>* b_particles;
 
-// O(n)
-Particle* find_furthest(vector<Particle*>* particles, int T)
+// O(N)
+Particle* find_furthest(vector<Particle*>* particles, double T)
 {
     Team team = particles->at(0)->team;
     Particle* furthest_particle = particles->at(0);
-    int distance_of_furthest = furthest_particle->position_at_time(T);
+    double distance_of_furthest = furthest_particle->position_at_time(T);
     for (Particle* current_particle : *particles)
     {   
         int current_distance = current_particle->position_at_time(T);
-        if (team == A ? current_distance > distance_of_furthest \
+        if (team == A ? current_distance > distance_of_furthest
                       : current_distance < distance_of_furthest)
         {
             furthest_particle = current_particle;
@@ -67,17 +77,18 @@ Particle* find_furthest(vector<Particle*>* particles, int T)
     return furthest_particle;
 }
 
-// O(n)
-tuple<Particle*, Particle*> furthest_particles(vector<Particle*>* a_particles, \
-                                               vector<Particle*>* b_particles, int T)
+tuple<Particle*, Particle*> furthest_particles(vector<Particle*>* a_particles,
+                                               vector<Particle*>* b_particles, double T)
 {
-    return make_tuple(find_furthest(a_particles, T), find_furthest(b_particles, T));
+    return make_tuple(find_furthest(a_particles, T), 
+                      find_furthest(b_particles, T));
 }
 
-tuple<Particle*, Particle*> binarySearch(vector<Particle*>* a_particles, \
-                                         vector<Particle*>* b_particles, int T)
+// O(NlogL)
+tuple<Particle*, Particle*> binarySearch(vector<Particle*>* a_particles,
+                                         vector<Particle*>* b_particles, double T)
 {
-    int t1 = 0, t2 = T, mid;
+    double t1 = 0, t2 = T, mid;
     tuple<Particle*, Particle*> ret;
     while (t1 <= t2)
     {
@@ -87,13 +98,14 @@ tuple<Particle*, Particle*> binarySearch(vector<Particle*>* a_particles, \
         if (distance == 0)
             break;
         if (distance > 0)
-            t2 = mid - 1;
+            t2 = mid - 0.001;
         if (distance < 0)
-            t1 = mid + 1;
+            t1 = mid + 0.001;
     }
     return ret;
 }                                         
 
+// O(KNlogL)
 int main(int argc, char const *argv[])
 {
     ios_base::sync_with_stdio(false);
@@ -126,14 +138,14 @@ int main(int argc, char const *argv[])
         cout << par->id << "\n";
 #endif
 
-    // find the time of first collision
     for (int k = 0; k < K; k++)
     {
-        tuple<Particle*, Particle*> pair = binarySearch(a_particles, b_particles, L);
+        tuple<Particle*, Particle*> pair = binarySearch(a_particles, 
+                                                        b_particles, L);
         cout << get<0>(pair)->id << " " << get<1>(pair)->id << "\n";
-        a_particles->erase(remove(a_particles->begin(), a_particles->end(), get<0>(pair)), \
+        a_particles->erase(remove(a_particles->begin(), a_particles->end(), get<0>(pair)),
                            a_particles->end());
-        b_particles->erase(remove(b_particles->begin(), b_particles->end(), get<1>(pair)), \
+        b_particles->erase(remove(b_particles->begin(), b_particles->end(), get<1>(pair)),
                            b_particles->end());
     }
 
