9be7adca636d3f0244c1788ae7377278c7abd664
Nick Gavalas <gavalnick@gmail.com>
2018-11-08 02:25:21 +0200

    still buggy but passes some testcases

--- ex1b/poleodomia.cpp
+++ ex1b/poleodomia.cpp
@@ -7,9 +7,9 @@
 
 using namespace std;
 
-int N;
 long long buildings[1000000];
 long long costs[1000000];
+int N;
 
 int main(int argc, char const *argv[])
 {
@@ -28,21 +28,22 @@ int main(int argc, char const *argv[])
     stack<pair<long long, int>> stack_left;
     stack_left.push(make_pair(buildings[0], 1));
     costs[0] = buildings[0];
+    long long sum = buildings[0];
     for (int i = 1; i < N; i++)
     {
-        long long sum = 0;
         pair<int, int> pr;
         if (buildings[i] > buildings[i - 1])
         {
-            int popped_count = 0;
+            int popped_count = 1;
             while (!stack_left.empty() && stack_left.top().first < buildings[i])
             {
                 pr = stack_left.top();
                 stack_left.pop();
+                sum -= pr.first * pr.second;
                 popped_count += pr.second;
             }
             stack_left.push(make_pair(buildings[i], popped_count));
-            sum += buildings[i];
+            sum += buildings[i] * popped_count;
         }
         else if (buildings[i] == buildings[i - 1])
         {
@@ -63,32 +64,33 @@ int main(int argc, char const *argv[])
     stack<pair<long long, int>> stack_right;
     stack_right.push(make_pair(buildings[N - 1], 1));
     costs[N - 1] += buildings[N - 1];
-    for (int i = N - 1; i > 0; i--)
+    sum = buildings[N - 1];
+    for (int i = N - 2; i >= 0; i--)
     {
-        long long sum = 0;
         pair<int, int> pr;
         if (buildings[i] > buildings[i + 1])
         {
-            int popped_count = 0;
-            while (!stack_left.empty() && stack_left.top().first < buildings[i])
+            int popped_count = 1;
+            while (!stack_right.empty() && stack_right.top().first < buildings[i])
             {
-                pr = stack_left.top();
-                stack_left.pop();
+                pr = stack_right.top();
+                stack_right.pop();
+                sum -= pr.first * pr.second;
                 popped_count += pr.second;
             }
-            stack_left.push(make_pair(buildings[i], popped_count));
-            sum += buildings[i];
+            stack_right.push(make_pair(buildings[i], popped_count));
+            sum += buildings[i] * popped_count;
         }
         else if (buildings[i] == buildings[i + 1])
         {
-            pr = stack_left.top();
-            stack_left.pop();
-            stack_left.push(make_pair(pr.first, pr.second + 1));
+            pr = stack_right.top();
+            stack_right.pop();
+            stack_right.push(make_pair(pr.first, pr.second + 1));
             sum += buildings[i];
         }
         else
         {
-            stack_left.push(make_pair(buildings[i], 1));
+            stack_right.push(make_pair(buildings[i], 1));
             sum += buildings[i];
         }
         costs[i] += sum;
