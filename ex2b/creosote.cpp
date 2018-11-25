04d45dbb24e3aabe5acb8f1f98460e38930c1505
Nick Gavalas <gavalnick@gmail.com>
2018-11-25 21:08:35 +0200

    finished ex2b 'creosote' (there is room for improvement)

--- ex2b/creosote.cpp
+++ ex2b/creosote.cpp
@@ -6,9 +6,9 @@ using namespace std;
 int N, a, b, c;
 
 int delicacies[10000];
-long long partial_sums[10000];
+long long partial_sums[10001];
 
-long long dp[10000];
+long long dp[10001];
 
 int main(int argc, char const *argv[])
 {
@@ -22,29 +22,29 @@ int main(int argc, char const *argv[])
         cin >> delicacies[i];
     }
 
+    partial_sums[0] = 0;
     long long ongoing_sum = 0;
-    for (int i = 0; i < N; i++)
+    for (int i = 1; i <= N; i++)
     {
-        ongoing_sum += delicacies[i];
+        ongoing_sum += delicacies[i - 1];
         partial_sums[i] = ongoing_sum;
     }
 
     dp[0] = 0;
-    dp[1] = delicacies[0];
-    for (int i = 2; i < N; i++)
+    for (int i = 1; i <= N; i++)
     {
         long long max = INT64_MIN;
-        for (int j = 1; j < i; j++)
+        for (int j = 1; j <= i; j++)
         {
             long long sums = partial_sums[i] - partial_sums[j - 1];
-            long long curr = dp[j - 1] + a * pow(sums, 2) + b * sums + c;
+            long long curr = dp[j - 1] + (a * pow(sums, 2)) + (b * sums) + c;
             if (curr > max)
                 max = curr;
         }
         dp[i] = max;
     }
 
-    cout << dp[N - 1] << "\n";
+    cout << dp[N] << "\n";
 
     return 0;
 }
