d2a88418beb7c0f4a55a77c1b2ec759e9f9e695c
Nik Gavalas <gavalnick@gmail.com>
2018-12-03 18:20:04 +0200

    progress on ex2b credits

--- ex2b/credits.cpp
+++ ex2b/credits.cpp
@@ -1,45 +1,89 @@
+// O(nlogn)
+
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
+int findCeilIndex(vector<int> &v, int key)
+{
+    int l = 0;
+    int r = v.size() - 1;
+
+    while (r - l > 1)
+    {
+        int m = l + (r - l) / 2;
+        if (v[m] >= key)
+            r = m;
+        else
+            l = m;
+    }
+
+    return r;
+}
+
+int findBottomIndex(vector<int> &v, int key)
+{
+    int l = 0;
+    int r = v.size() - 1;
+
+    while (r - l > 1)
+    {
+        int m = l + (r - l) / 2;
+        if (v[m] <= key)
+            r = m;
+        else
+            l = m;
+    }
+
+    return r;
+}
 
 int main(int argc, char const *argv[])
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int N;
+    size_t N;
     cin >> N;
 
     int *creds = new int[N];
-    for (int i = 0; i < N; i++)
+    for (size_t i = 0; i < N; i++)
         cin >> creds[i];
 
-    int *dp = new int[N];
-    for (int i = 0; i < N; i++)
-        dp[i] = 0;
-
-    int max_cred = creds[N - 1];
-    for (int i = N - 2; i >= 0; i--)
+    int *left = new int[N];
+    vector<int> left_aggr;
+    for (size_t i = 0; i < N; i++)
     {
-        if (creds[i] > max_cred)
-            max_cred = creds[i];
+        if (left_aggr.empty() || creds[i] > left_aggr[left_aggr.size() - 1])
+            left_aggr.push_back(creds[i]);
+        else
+            left_aggr[findCeilIndex(left_aggr, creds[i])] = creds[i];
 
-        dp[i] = max(dp[i + 1], max_cred - creds[i]);
+        left[i] = left_aggr.size();
     }
 
-    int min_cred = creds[0];
-    for (int i = 1; i < N; i++)
+    int *right = new int[N];
+    vector<int> right_aggr;
+    for (size_t i = N - 1; i >= 0; i--)
     {
-        if (creds[i] < min_cred)
-            min_cred = creds[i];
+        if (right_aggr.empty() || creds[i] < right_aggr[right_aggr.size() - 1])
+            right_aggr.push_back(creds[i]);
+        else
+            right_aggr[findBottomIndex(right_aggr, creds[i])] = creds[i];
 
-        dp[i] = max(dp[i - 1], dp[i] + creds[i] - min_cred);
+        right[i] = right_aggr.size();
     }
 
-    cout << dp[N - 1] << "\n";
+    for (size_t i = 0; i < N; i++)
+        cout << left[i] << ", ";
+    cout << "\n";
+
+    for (size_t i = 0; i < N; i++)
+        cout << right[i] << ", ";
+    cout << "\n";
 
     //delete [] creds;
-    //delete [] dp;
 
     return 0;
 }
