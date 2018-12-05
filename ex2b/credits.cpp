7e89335cccea3a89ff09166e3f69b26b4c25d2ed
Nick Gavalas <gavalnick@gmail.com>
2018-12-05 12:40:59 +0200

    small advancements ex2b

--- ex2b/credits.cpp
+++ ex2b/credits.cpp
@@ -2,43 +2,39 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
-//#define DEBUG
+#define DEBUG
 
 using namespace std;
 
-int findCeilIndex(vector<int> &v, int key)
-{
-    int l = 0;
-    int r = v.size() - 1;
+vector<int> creds(500000);
+vector<int> left_lis(500000);
+vector<int> right_lis(500000);
 
-    while (r - l > 1)
-    {
-        int m = l + (r - l) / 2;
-        if (v[m] >= key)
-            r = m;
-        else
-            l = m;
-    }
-
-    return r;
-}
-
-int findBottomIndex(vector<int> &v, int key)
-{
-    int l = 0;
-    int r = v.size() - 1;
-
-    while (r - l > 1)
-    {
-        int m = l + (r - l) / 2;
-        if (v[m] < key)
-            r = m;
-        else
-            l = m;
-    }
-
-    return r;
+int LongestIncreasingSubsequenceLength(std::vector<int>& v) 
+{ 
+    if (v.size() == 0) 
+        return 0; 
+  
+    std::vector<int> tail(v.size(), 0); 
+    int length = 1; 
+  
+    tail[0] = v[0]; 
+    for (size_t i = 1; i < v.size(); i++) { 
+        if (v[i] > tail[length - 1]) 
+            tail[length++] = v[i]; 
+        else { 
+            auto it = find(tail.begin(), tail.begin() + length, v[i]); 
+            if (it != tail.begin() + length) { 
+                continue; 
+            } 
+            it = upper_bound(tail.begin(), tail.begin() + length, v[i]); 
+            *it = v[i]; 
+        } 
+    } 
+  
+    return length; 
 }
 
 int main(int argc, char const *argv[])
@@ -49,57 +45,44 @@ int main(int argc, char const *argv[])
     size_t N;
     cin >> N;
 
-    int *creds = new int[N];
-    for (size_t i = 0; i < N; i++)
-        cin >> creds[i];
-
-    int *left = new int[N];
-    vector<int> left_aggr;
+    int read;
     for (size_t i = 0; i < N; i++)
     {
-        if (left_aggr.empty() || creds[i] > left_aggr[left_aggr.size() - 1])
-            left_aggr.push_back(creds[i]);
-        else
-            left_aggr[findCeilIndex(left_aggr, creds[i])] = creds[i];
-
-        left[i] = left_aggr.size();
+        cin >> read;
+        creds.push_back(read);
     }
 
-    int *right = new int[N];
-    vector<int> right_aggr;
-    for (size_t i = N; i > 0; i--)
+    vector<int> left_aggr;
+    left_aggr.push_back(creds[0]);
+    for (size_t i = 1; i < N; i++)
     {
-        if (right_aggr.empty() || creds[i - 1] < right_aggr[right_aggr.size() - 1])
-            right_aggr.push_back(creds[i - 1]);
+        if (creds[i] > left_aggr[left_aggr.size() - 1])
+            left_aggr.push_back(creds[i]);
         else
-            right_aggr[findBottomIndex(right_aggr, creds[i - 1])] = creds[i - 1];
-
-        right[i - 1] = right_aggr.size();
+        {
+            // TO check whether the element is not present before hand 
+            /* auto it = find(left_aggr.begin(), left_aggr.end(), creds[i]); 
+            if (it != left_aggr.end()) { 
+                continue; 
+            }  */
+            // If not present change the tail element to v[i] 
+            auto it = upper_bound(left_aggr.begin(), left_aggr.end(), creds[i]); 
+            *it = creds[i]; 
+        }
+        left_lis[i] = left_aggr.size();
     }
 
 #ifdef DEBUG
     for (size_t i = 0; i < N; i++)
-        cout << left[i] << ", ";
+        cout << left_lis[i] << ", ";
     
-    cout << "\n\n";
+    cout << "\n";
 
-    for (size_t i = 0; i < N; i++)
-        cout << right[i] << ", ";
+/*     for (size_t i = 0; i < N; i++)
+        cout << left_lis[i] << ", ";
+    
+    cout << "\n"; */
 #endif
-
-    int ret = 0;
-    for (size_t i = 0; i < N - 1; i++)
-    {
-        int sum = left[i] + right[i + 1]; 
-        if (sum > ret)
-            ret = sum;
-    }
-    if (ret < left[N - 1])
-        ret = left[N - 1];
-    if (ret < left[0])
-        ret = left[0];
-
-    cout << ret << "\n";
 
     //delete [] creds;
 
