ecbe2106ce70391227152ba54b4978171238058a
Nick Gavalas <gavalnick@gmail.com>
2018-12-05 13:59:18 +0200

    ex2b finished

--- ex2b/credits.cpp
+++ ex2b/credits.cpp
@@ -4,38 +4,13 @@
 #include <vector>
 #include <algorithm>
 
-#define DEBUG
+//#define DEBUG
 
 using namespace std;
 
 vector<int> creds(500000);
 vector<int> left_lis(500000);
 vector<int> right_lis(500000);
-
-int LongestIncreasingSubsequenceLength(std::vector<int>& v) 
-{ 
-    if (v.size() == 0) 
-        return 0; 
-  
-    std::vector<int> tail(v.size(), 0); 
-    int length = 1; 
-  
-    tail[0] = v[0]; 
-    for (size_t i = 1; i < v.size(); i++) { 
-        if (v[i] > tail[length - 1]) 
-            tail[length++] = v[i]; 
-        else { 
-            auto it = find(tail.begin(), tail.begin() + length, v[i]); 
-            if (it != tail.begin() + length) { 
-                continue; 
-            } 
-            it = upper_bound(tail.begin(), tail.begin() + length, v[i]); 
-            *it = v[i]; 
-        } 
-    } 
-  
-    return length; 
-}
 
 int main(int argc, char const *argv[])
 {
@@ -49,27 +24,38 @@ int main(int argc, char const *argv[])
     for (size_t i = 0; i < N; i++)
     {
         cin >> read;
-        creds.push_back(read);
+        creds[i] = read;
     }
 
     vector<int> left_aggr;
     left_aggr.push_back(creds[0]);
+    left_lis[0] = 1;
     for (size_t i = 1; i < N; i++)
     {
         if (creds[i] > left_aggr[left_aggr.size() - 1])
             left_aggr.push_back(creds[i]);
         else
         {
-            // TO check whether the element is not present before hand 
-            /* auto it = find(left_aggr.begin(), left_aggr.end(), creds[i]); 
-            if (it != left_aggr.end()) { 
-                continue; 
-            }  */
-            // If not present change the tail element to v[i] 
             auto it = upper_bound(left_aggr.begin(), left_aggr.end(), creds[i]); 
             *it = creds[i]; 
         }
         left_lis[i] = left_aggr.size();
+    }
+
+    vector<int> right_aggr;
+    right_aggr.push_back(creds[N - 1]);
+    right_lis[N - 1] = 1;
+    for (size_t i = N - 1; i > 0; i--)
+    {
+        if (creds[i - 1] < right_aggr[right_aggr.size() - 1])
+            right_aggr.push_back(creds[i - 1]);
+        else
+        {
+            auto it = lower_bound(right_aggr.begin(), right_aggr.end(),
+                creds[i - 1], greater<int>()); 
+            *it = creds[i - 1];
+        }
+        right_lis[i - 1] = right_aggr.size();
     }
 
 #ifdef DEBUG
@@ -78,13 +64,21 @@ int main(int argc, char const *argv[])
     
     cout << "\n";
 
-/*     for (size_t i = 0; i < N; i++)
-        cout << left_lis[i] << ", ";
+    for (size_t i = 0; i < N; i++)
+        cout << right_lis[i] << ", ";
     
-    cout << "\n"; */
+    cout << "\n";
 #endif
 
-    //delete [] creds;
+    int ret = 0;
+    for (size_t i = 0; i < N - 1; i++)
+    {
+        int sum = left_lis[i] + right_lis[i + 1]; 
+        if (sum > ret)
+            ret = sum;
+    }
+
+    cout << ret << "\n";
 
     return 0;
 }
