de91bff80914fa3e32562e9939ea6c95ada36040
Nik Gavalas <gavalnick@gmail.com>
2019-01-05 21:02:40 +0200

    finish ex3b submarine

--- ex3b/submarine.cpp
+++ ex3b/submarine.cpp
@@ -1,17 +1,15 @@
+// O(N * M * K)
+
 #include <iostream>
+#include <utility>
 
 #define KEY 1000000103
 
 using namespace std;
 
-typedef struct {
-    long long paths[102];
-    int pilot;
-    int x;
-    int y;
-} node;
-
-node grid[180][180];
+long long grid[180][180][101];
+bool is_passage[180][180];
+pair<int, int> exits[180][180];
 int N, M, K, X;
 
 int main(int argc, char const *argv[])
@@ -21,30 +19,33 @@ int main(int argc, char const *argv[])
 
     cin >> N >> M >> K >> X;
 
-    grid[M - 1][N - 1].paths[0] = 1;
+    grid[M - 1][N - 1][0] = 1;
 
-    int s, e, temp_x, temp_y;
+    // sexy solution
+    int s, e, x, y;
     for (int i = 0; i < K; i++) {
         cin >> s >> e;
-        temp_x = s / M;
-        temp_y = s % M;
-        grid[temp_y][temp_x].pilot = 1;
-        grid[temp_y][temp_x].x = e / M;
-        grid[temp_y][temp_x].y = e % M;
+
+        x = s / M;
+        y = s % M;
+        is_passage[y][x] = true;
+        exits[y][x] = make_pair(e / M, e % M);
     }
 
     for (int i = M - 1; i >= 0; i--) {
         for (int j = N - 1; j >= 0; j--) {
             for (int k = 0; k <= X; k++) {
-                int val = grid[i][j].paths[k] % KEY;
+                int paths = grid[i][j][k] % KEY;
 
-                if (grid[i][j].pilot == 1)
-                    grid[grid[i][j].y][grid[i][j].x].paths[k + 1] += val;
+                if (is_passage[i][j]) {
+                    auto ext = exits[i][j];
+                    grid[ext.second][ext.first][k + 1] += paths;
+                }
                 else {
                     if (i != 0)
-                        grid[i - 1][j].paths[k] += val;
+                        grid[i - 1][j][k] += paths;
                     if (j != 0)
-                        grid[i][j - 1].paths[k] += val;
+                        grid[i][j - 1][k] += paths;
                 }
             }
         }
@@ -52,7 +53,7 @@ int main(int argc, char const *argv[])
 
     long long res = 0;
     for (int k = 0; k <= X; k++) {
-        res += grid[0][0].paths[k] % KEY;
+        res += grid[0][0][k] % KEY;
     }
 
     cout << res % KEY << "\n";
