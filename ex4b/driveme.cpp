f729641f429d31d191b308b25cb30b20e1262031
Nik Gavalas <gavalnick@gmail.com>
2019-02-20 14:50:13 +0200

    advancements in ex4b driveme exer.

--- ex4b/driveme.cpp
+++ ex4b/driveme.cpp
@@ -1,14 +1,15 @@
 #include <assert.h>
 
-#include <iostream>
-#include <utility>
-#include <list>
-#include <vector>
-#include <queue>
 #include <algorithm>
 #include <climits>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <utility>
+#include <vector>
 
-// TODO: remove this
+// I know this is not a good practice but saves typing
 using namespace std;
 
 typedef pair<int, int> P;
@@ -18,7 +19,7 @@ class Graph {
     list<P> *adj;
     vector<bool> visited;
 
-public:
+   public:
     Graph(int V) {
         this->V = V;
         this->adj = new list<P>[V];
@@ -26,6 +27,8 @@ public:
     }
 
     void add_edge(int source, int dest, int dist) {
+        assert(source < this->V);
+        assert(dest < this->V);
         this->adj[source].push_back(make_pair(dest, dist));
     }
 
@@ -34,9 +37,11 @@ public:
         assert(this->V == (int)results.size());
 
         priority_queue<P, vector<P>, greater<P>> q;
+        // reset vector
+        fill(this->visited.begin(), this->visited.end(), false);
         // (distance from source vertex, current vertex) pair
         int dist_from_source = 0;
-        int curr_vertex = 0;
+        int curr_vertex = source;
         results[source] = 0;
         q.push(make_pair(dist_from_source, curr_vertex));
 
@@ -46,7 +51,7 @@ public:
             // additions for readability
             dist_from_source = popped.first;
             curr_vertex = popped.second;
-            
+
             if (!this->visited[curr_vertex]) {
                 this->visited[curr_vertex] = true;
                 for (auto &neighbor : this->adj[curr_vertex]) {
@@ -74,24 +79,43 @@ int main(int argc, char const *argv[]) {
     //TODO: replace postfix with prefix ++
     for (int i = 0; i < M; i++) {
         cin >> source >> dest >> dist;
-        // TODO: cleanup this mess here
+        cout << source << " " << dest << " " << dist << "\n"; //
+        // adjustments for zero indexing
+        source--;
+        dest--;
         for (int k = 0; k <= K; k++) {
+            int step = k * N;
             // add all forward edges
-            graph.add_edge(source + (k * N) - 1, dest + (k * N) - 1, dist);
+            graph.add_edge(source + step, dest + step, dist);
             // add heper backward edges
             if (k != K)
-                graph.add_edge(dest + (k * N) - 1, source + ((k + 1) * N) - 1, dist);
+                graph.add_edge(dest + step, source + step + N, dist);
         }
     }
+    cout << "\n"; //
 
-    vector<int> results = vector<int>(N * (K + 1), INT_MAX);
-    graph.find_shortest_distances(0, results);
-    for (auto &dist : results)
-        cout << dist << "\n";
+    // source = 5;
+    // vector<int> results = vector<int>(N * (K + 1), INT_MAX);
+    // graph.find_shortest_distances(source - 1, results);
+    // for (auto &dist : results)
+    //     cout << dist << "\n";
 
     int num_offenses;
+    map<int, vector<int>> cache;
     for (int i = 0; i < Q; i++) {
         cin >> source >> dest >> num_offenses;
+        cout << source << " " << dest << " " << num_offenses << " | "; // DEBUG
+        source--;
+        dest--;
+        if (cache.count(source) == 0) {
+            cache[source] = vector<int>(N * (K + 1), INT_MAX);
+            graph.find_shortest_distances(source, cache[source]);
+        }
+        int res = cache[source][dest + (num_offenses * N)];
+        if (res == INT_MAX)
+            cout << "IMPOSSIBLE" << "\n";
+        else
+            cout << res << "\n";
     }
 
     return 0;
